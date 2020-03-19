f384038ae53a0fee0afbfe6af91fc2968f6fe82e
alexist98 <alexistoigo@gmail.com>
2020-03-19 00:40:53 -0300

    removido imports inutlizados da home_tab

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:transparent_image/transparent_image.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
