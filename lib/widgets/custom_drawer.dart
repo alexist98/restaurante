173f03ea9cc9ec50a45158d1ac8b8f2a8438e09e
alexist98 <alexistoigo@gmail.com>
2020-03-19 13:34:52 -0300

    adicionado texto(nome, ola entre/cadraste) ao drawer e iniciado o botao entre ou cadraste-se.

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -3,6 +3,73 @@ import 'package:flutter/material.dart';
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget buildDrawerBack() =>
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                //cores tela drawer
+                Color.fromARGB(255, 154, 188, 241),
+                Colors.white
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        );
+
+    return Drawer(
+      child: Stack(
+        children: <Widget>[
+          buildDrawerBack(),
+          ListView(
+            padding: EdgeInsets.only(left: 32.0, top: 16.0),
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 8.0),
+                padding: EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 8.0),
+                height: 170.0,
+                child: Stack(
+                  children: <Widget>[
+                    Positioned(
+                      top: 8.0,
+                      left: 0.0,
+                      child: Text(
+                        "Erechim\nLanches",
+                        style: TextStyle(
+                            fontSize: 48.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Positioned(
+                      left: 0.0,
+                      bottom: 0.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Olá, ",
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                          GestureDetector(
+                            child: Text(
+                              "Entre ou cadastre-se",
+                              style: TextStyle(
+                                  color: Theme
+                                      .of(context)
+                                      .primaryColor,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            onTap: () {
+
+                            },
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
