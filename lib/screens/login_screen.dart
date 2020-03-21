f38e5a3204d554a1aa5bd47e15d190c4b47bd87a
alexist98 <alexistoigo@gmail.com>
2020-03-21 16:29:05 -0300

    tela login com box de email, senha e botão entrar.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,6 +20,52 @@ class LoginScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "E-mail",
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16.0,),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Senha",
+              ),
+              obscureText: true,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FlatButton(
+                onPressed: () {},
+                child: Text("Esqueci minha senha",
+                  textAlign: TextAlign.right,
+                ),
+                padding: EdgeInsets.zero,
+              ),
+            ),
+            SizedBox(height: 16.0,),
+            SizedBox(
+              height: 44.0,
+              child: RaisedButton(
+                child: Text("Entrar",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                textColor: Colors.white,
+                color: Theme
+                    .of(context)
+                    .primaryColor,
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
